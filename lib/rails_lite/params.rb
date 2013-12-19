d648012439aebc11a3d4aeff4087489343b43642
Brian McGue <mcgue.brian@gmail.com>
2013-12-18 17:50:13 -0800

    finish phase V, used recursive solution to build params

--- lib/rails_lite/params.rb
+++ lib/rails_lite/params.rb
@@ -2,7 +2,9 @@ require 'uri'
 
 class Params
   def initialize(req, route_params = {})
-    parse_www_encoded_form(req.query_string)
+    @params = {}
+    parse_www_encoded_form(req.query_string) if req.query_string
+    parse_www_encoded_form(req.body) if req.body
   end
 
   def [](key)
@@ -10,17 +12,28 @@ class Params
   end
 
   def to_s
-    @params.to_s
+    @params.to_json.to_s
   end
 
   private
+  def build_nested_param_hash(key_arr, value, hash)
+    if key_arr.count == 1
+      hash[key_arr.first] = value
+      return hash
+    end
+    hash[key_arr.first] ||= {}
+    build_nested_param_hash(key_arr[1..-1], value, hash[key_arr.first])
+  end
+
   def parse_www_encoded_form(www_encoded_form)
-    @params = {}
     URI::decode_www_form(www_encoded_form).each do |pair|
-      @params[pair.first] = pair.last
+      nested_key, value = pair
+      key_arr = parse_key(nested_key)
+      build_nested_param_hash(key_arr, value, @params)
     end
   end
 
   def parse_key(key)
+    key.split(/\]\[|\[|\]/)
   end
 end
